eb1da58fdca7b794a25c7758996ce2e23b9b381d
nickitaUrbanovich <nick.urbanovich@mail.ru>
2022-12-20 10:17:05 +0300

    da end of services and main page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,1 @@
-"use strict";baguetteBox.run(".gallery");
+"use strict";var loadData=function(){return new Promise(function(e,t){setTimeout(e,5e3)})};loadData().then(function(){var e=document.getElementById("preloader");e.classList.add("hidden"),e.classList.remove("visible")}),baguetteBox.run(".gallery");
