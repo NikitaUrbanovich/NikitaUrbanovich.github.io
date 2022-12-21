3c2f71c56a3cc8381324257cb04a662014508804
nickitaUrbanovich <nick.urbanovich@mail.ru>
2022-12-21 10:37:05 +0300

    last changes in site and ready

--- js/script.js
+++ js/script.js
@@ -1,1 +1,1 @@
-"use strict";var loadData=function(){return new Promise(function(e,t){setTimeout(e,3e3)})};loadData().then(function(){var e=document.getElementById("preloader");e.classList.add("hidden"),e.classList.remove("visible")}),baguetteBox.run(".gallery");
+"use strict";var loadData=function(){return new Promise(function(e,t){setTimeout(e,3e3)})};loadData().then(function(){var e=document.getElementById("preloader");e.classList.add("hidden"),e.classList.remove("visible")}),baguetteBox.run(".gallery",{noScrollbars:!0,buttons:!0});
